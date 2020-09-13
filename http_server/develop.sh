22a2e7bf477dba3e498be576c15fa5c506a1b663
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-09-12 20:05:37 -0500

    Bash script

--- http_server/develop.sh
+++ http_server/develop.sh
@@ -1,39 +1,19 @@
-import os
-import http.server
-import socketserver
-import json
+#!/bin/bash
+docker build \
+--file=Dockerfile \
+--tag="http_server:0.1" \
+.
 
-from urllib.request import Request, urlopen
+docker create \
+--interactive \
+--tty \
+--rm \
+--name=http_server \
+--publish-all \
+--mount type=bind,source="${pwd}/src",target=/home/http_server_user/src \
+--workdir /home/http_server_user/src \
+http_server:0.1
 
-HTTP_PORT = int(os.environ.get("PORT"))
-API_SERVER_URL = os.environ.get("API_SERVER_URL")
-
-
-class RequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-        html = """
-            <html>
-                <body>
-                    <h1>{message}</h1>
-                </body>
-            </html>
-        """
-
-        if API_SERVER_URL is not None:
-            url = f"{API_SERVER_URL}/person"
-            response = urlopen(Request(url))
-            person = json.loads(response.read().decode())
-            message = f"Hello from {person['name']} {person['surname']}"
-        else:
-            message = "No connection to API Server. API_SERVER_URL not defined!"
-
-        self.wfile.write(bytes(html.format(message=message), "utf-8"))
-
-        return
-
-
-socketserver.TCPServer(("", HTTP_PORT), RequestHandler).serve_forever()
+docker start http_server
+docker port http_server
+docker attach http_server
